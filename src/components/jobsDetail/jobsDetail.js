d0b949b5ee457e318ed99e30dee2a45dc274c3ab
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-12-19 02:40:51 +0700

    Implement: Get Data

--- src/components/jobsDetail/jobsDetail.js
+++ src/components/jobsDetail/jobsDetail.js
@@ -1,18 +1,45 @@
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react';
 import JobCardDesc from './jobCard';
-import jobData from './fake.json';
 
-const JobsDetail = () => {
+const JobsDetail = ({ data }) => {
   return (
     <div>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Grid container style={{ marginLeft: 128 }}>
+      <style>
+        {`
+          li {
+            list-style: inside;
+            padding-top: 1rem;
+          }
+
+          ol, ul {
+            padding-left: 1rem;
+          }
+        `}
+      </style>
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          textAlign: 'left',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Grid container spacing={7} style={{ marginLeft: 90 }}>
           <Grid item xs={6.5}>
-            <Box
-              sx={{ paddingRight: 2 }}
-              dangerouslySetInnerHTML={{ __html: jobData.job1.jobDesc }}
-            ></Box>
+            <Grid item xs={4} sx={{ fontSize: 18, fontWeight: 500 }}>
+              <Box sx={{ marginBottom: '10px' }}>Nội dung công việc</Box>
+            </Grid>
+            <Box dangerouslySetInnerHTML={{ __html: data.description }}></Box>
+            <Grid item xs={4} sx={{ fontSize: 18, fontWeight: 500 }}>
+              <Box sx={{ marginLeft: 0 }}>Yêu cầu công việc</Box>
+            </Grid>
+            <Box dangerouslySetInnerHTML={{ __html: data.requirements }}></Box>
+            <Grid item xs={4} sx={{ fontSize: 18, fontWeight: 500 }}>
+              <Box sx={{ marginLeft: 0 }}>Quyền lợi</Box>
+            </Grid>
+            <Box dangerouslySetInnerHTML={{ __html: data.advantages }}></Box>
           </Grid>
 
           <Grid item>
